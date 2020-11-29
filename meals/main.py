57110b1b444e93e3a50579c2a7770b4038bcea78
dangdan1ing15 <kimsunghun01029301134@gmail.com>
2020-11-30 03:22:32 +0900

    BMI & KCAL FIN

--- meals/main.py
+++ meals/main.py
@@ -16,22 +16,29 @@ class MainPage(QWidget):
 
         # Left side
         # Group
-        self.BMI_Checker = QGroupBox('BMI/기초대사량 계산기') # Group for BMI_Height, BMI_Weight, BMI_Confirm
+        self.BMI_Checker = QGroupBox('BMI/기초대사량 계산기') # Group for BMI_Gender, BMI_Age, BMI_Height, BMI_Weight, BMI_Confirm
         self.BMI_Show = QGroupBox('BMI') # Group for BMI_Label1, BMI_Label2, BMI_Figure
         self.Kcal_Show = QGroupBox('Kcal') # Group for Kcal_Label1, Kcal_label2, Kcal_Figure
         # BMI_Checker Group
-        self.BC_label = QLabel('키&몸무게 입력')
+        self.BC_label = QLabel('사전정보 입력')
+        self.BMI_Gender_Male = QCheckBox('남성')
+        self.BMI_Gender_Female = QCheckBox('여성')
+        self.BMI_Age = QLineEdit()
+        self.BMI_Age.setPlaceholderText('나이(세)')
         self.BMI_Height = QLineEdit()
         self.BMI_Height.setPlaceholderText('키(cm)')
         self.BMI_Weight = QLineEdit()
         self.BMI_Weight.setPlaceholderText('몸무게(kg)')
-        self.BMI_Confirm = QPushButton('확인')
+        self.BMI_Confirm = QPushButton('제출')
         # Set BMI_Checker layout
         self.BC_layout = QGridLayout()
         self.BC_layout.addWidget(self.BC_label, 0, 0)
-        self.BC_layout.addWidget(self.BMI_Height, 1, 0, 2, 3)
-        self.BC_layout.addWidget(self.BMI_Weight, 3, 0, 2, 3)
-        self.BC_layout.addWidget(self.BMI_Confirm, 5, 0, 2, 3)
+        self.BC_layout.addWidget(self.BMI_Gender_Male, 1, 0)
+        self.BC_layout.addWidget(self.BMI_Gender_Female, 1, 1)
+        self.BC_layout.addWidget(self.BMI_Age, 2, 0, 2, 3)
+        self.BC_layout.addWidget(self.BMI_Height, 4, 0, 2, 3)
+        self.BC_layout.addWidget(self.BMI_Weight, 6, 0, 2, 3)
+        self.BC_layout.addWidget(self.BMI_Confirm, 8, 0, 2, 3)
         self.BMI_Checker.setLayout(self.BC_layout)
         # BMI_Show Group
         self.BMI_Label1 = QLabel('당신의 BMI는')
@@ -41,17 +48,17 @@ class MainPage(QWidget):
         self.BS_layout = QGridLayout()
         self.BS_layout.addWidget(self.BMI_Label1, 1, 0)
         self.BS_layout.addWidget(self.BMI_Figure, 3, 0)
-        self.BS_layout.addWidget(self.BMI_Label2, 3, 3)
+        self.BS_layout.addWidget(self.BMI_Label2, 3, 1)
         self.BMI_Show.setLayout(self.BS_layout)
-        # BMI_Show Group
+        # Kcal_Show Group
         self.Kcal_Label1 = QLabel('당신의 기초대사량은')
-        self.Kcal_Figure = QLabel()
+        self.Kcal_Figure = QLabel('0')
         self.Kcal_Label2 = QLabel('입니다.')
-        # Set BMI_Show layout
+        # Set Kcal_Show layout
         self.KS_layout = QGridLayout()
         self.KS_layout.addWidget(self.Kcal_Label1, 1, 0)
         self.KS_layout.addWidget(self.Kcal_Figure, 3, 0)
-        self.KS_layout.addWidget(self.Kcal_Label2, 3, 3)
+        self.KS_layout.addWidget(self.Kcal_Label2, 3, 1)
         self.Kcal_Show.setLayout(self.KS_layout)
 
         # Right side
@@ -98,21 +105,48 @@ class MainPage(QWidget):
         self.setWindowTitle('오늘의 식단')
 
         # Signals
-        self.BMI_Confirm.clicked.connect(self.BMICalculatingFunction)
-
-    def BMICalculatingFunction(self):#if 조건으로 아무것도 안쳐질때 오류메시지 출력하
+        self.BMI_Confirm.clicked.connect(self.ShowLeftsideFunction)
+        self.BMI_Gender_Male.stateChanged.connect(self.gendercheckBoxState)
+        self.BMI_Gender_Female.stateChanged.connect(self.gendercheckBoxState)
+    def gendercheckBoxState(self):
+        gendercounter = 0
+        if self.BMI_Gender_Male.isChecked() == True:
+            gendercounter = 1
+        if self.BMI_Gender_Female.isChecked() == True:
+            gendercounter = 2
+        return gendercounter
+    def BMICalculatingFunction(self):#if 조건으로 아무것도 안쳐질때 오류메시지 출력하는 함수로 변경하기
         height = self.BMI_Height.text()
         weight = self.BMI_Weight.text()
         heights = int(height)
         weights = int(weight)
         BMI = weights/((heights/100)*(heights/100))
-
-        # debug calculating BMI
-        print(round(BMI, 2))
+        return BMI
+    def KcalCalculatingFunction(self):
+        gendercounter = self.gendercheckBoxState()
+        age = self.BMI_Age.text()
+        height = self.BMI_Height.text()
+        weight = self.BMI_Weight.text()
+        ages = int(age)
+        heights = int(height)
+        weights = int(weight)
+        if gendercounter == 1:
+            Kcal = 66.47 + (13.75 * weights) + (5 * heights) - (6.76 * ages)
+            return Kcal
+        if gendercounter == 2:
+            Kcal = 655.1 + (9.56 * weights) + (1.85 * heights) - (4.68 * ages)
+            return Kcal
+        else:
+            return 0
     def ShowBMIFunction(self):
         BMI = self.BMICalculatingFunction()
-        self.BMI_Figure.setText(round(BMI, 2))
-
+        self.BMI_Figure.setNum(round(BMI,2))
+    def ShowKcalFunction(self):
+        Kcal = self.KcalCalculatingFunction()
+        self.Kcal_Figure.setNum(int(Kcal))
+    def ShowLeftsideFunction(self):
+        self.ShowBMIFunction()
+        self.ShowKcalFunction()
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     Main = MainPage()
