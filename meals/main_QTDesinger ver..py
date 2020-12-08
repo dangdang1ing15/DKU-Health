c399318fb069f16c9fb384ec6649e1443586e67e
dangdan1ing15 <kimsunghun01029301134@gmail.com>
2020-12-08 22:17:09 +0900

    ㄱ완전끝

--- meals/main_QTDesinger ver..py
+++ meals/main_QTDesinger ver..py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 
-class MainPage(QWidget):
+class MainPage(QWidget): # Main page Class
 
     def __init__(self):
         super().__init__()
@@ -67,23 +67,22 @@ class MainPage(QWidget):
         # Group
         self.Food_Group = QGroupBox('음식리스트') # Group for Food_List, Food_Adding
         # Food_Group
-        self.Food_List = QListWidget()
-        # Set Food_Group layout
-        self.FG_layout = QGridLayout()
-        self.FG_layout.addWidget(self.Food_List, 3, 0, 8, 5)
-        self.Food_Group.setLayout(self.FG_layout)
-        #FA
         self.FA_Group = QGroupBox('음식추가')
+        self.Food_List = QListWidget()
+        # FA_Group
         self.Food_name = QLineEdit()
         self.Food_name.setPlaceholderText('음식 이름')
         self.Food_Submit = QPushButton('추가')
         self.Food_Delete = QPushButton('삭제')
-        #
+        # Set Food_Group layout
+        self.FG_layout = QGridLayout()
+        self.FG_layout.addWidget(self.Food_List, 3, 0, 8, 5)
+        self.Food_Group.setLayout(self.FG_layout)
+        # Set FA_Group layout
         FA_layout = QGridLayout()
         FA_layout.addWidget(self.Food_name, 0, 0)
         FA_layout.addWidget(self.Food_Submit, 0, 1)
         FA_layout.addWidget(self.Food_Delete, 0, 2)
-
         self.FA_Group.setLayout(FA_layout)
 
         # Down side
@@ -115,12 +114,13 @@ class MainPage(QWidget):
         self.layout.addWidget(self.Food_Group, 1, 1, 3, 1)
         self.layout.addWidget(self.FA_Group, 0, 1)
         self.layout.addWidget(self.Weaklymeal_Group, 5, 0, 1, 2)
-
         self.setLayout(self.layout)
 
+        # extra window setting
+        self.resize(780, 730)
         self.setWindowTitle('오늘의 식단')
 
-        # Signals
+        # Signals, This makes widget to function
         self.BMI_Confirm.clicked.connect(self.ShowLeftsideFunction)
         self.BMI_Gender_Male.stateChanged.connect(self.gendercheckBoxState)
         self.BMI_Gender_Female.stateChanged.connect(self.gendercheckBoxState)
@@ -128,21 +128,23 @@ class MainPage(QWidget):
         self.Food_Delete.clicked.connect(self.DeleteListFunction)
         self.Weaklymeal_Generator.clicked.connect(self.generateFunction)
 
-    def gendercheckBoxState(self):
+    def gendercheckBoxState(self):# checking gender
         gendercounter = 0
         if self.BMI_Gender_Male.isChecked() == True:
             gendercounter = 1
         if self.BMI_Gender_Female.isChecked() == True:
             gendercounter = 2
         return gendercounter
-    def BMICalculatingFunction(self):#if 조건으로 아무것도 안쳐질때 오류메시지 출력하는 함수로 변경하기
+
+    def BMICalculatingFunction(self): # calculating BMI
         height = self.BMI_Height.text()
         weight = self.BMI_Weight.text()
         heights = int(height)
         weights = int(weight)
         BMI = weights/((heights/100)*(heights/100))
         return BMI
-    def KcalCalculatingFunction(self):
+
+    def KcalCalculatingFunction(self): # calculating Kcal
         gendercounter = self.gendercheckBoxState()
         age = self.BMI_Age.text()
         height = self.BMI_Height.text()
@@ -158,29 +160,33 @@ class MainPage(QWidget):
             return Kcal
         else:
             return 0
-    def ShowBMIFunction(self):
+
+    def ShowBMIFunction(self): # Bring BMI from BMICalculatingFunction and set BMI to BMI_Figure
         BMI = self.BMICalculatingFunction()
         self.BMI_Figure.setNum(round(BMI,2))
-    def ShowKcalFunction(self):
+
+    def ShowKcalFunction(self): # Bring Kcal from KcalCalculatingFunction and set Kcal to Kcal_Figure
         Kcal = self.KcalCalculatingFunction()
         self.Kcal_Figure.setNum(int(Kcal))
-    def ShowLeftsideFunction(self):
+
+    def ShowLeftsideFunction(self): # When Button Clicked, this running and processing BMI and Kcal
         self.ShowBMIFunction()
         self.ShowKcalFunction()
-    def AddListFunction(self):
+
+    def AddListFunction(self):  # When Food_Submit Clicked, Add lineedit to list
         self.foodText = self.Food_name.text()
         self.Food_List.addItem(self.foodText)
         self.Food_name.setText("")
-    def DeleteListFunction(self):
+
+    def DeleteListFunction(self): # When Food_Delete Clicked, Delete selected list
         self.removeItemRow = self.Food_List.currentRow()
         self.Food_List.takeItem(self.removeItemRow)
-    def generateFunction(self):
+
+    def generateFunction(self): # Adding random meal sheet
         selectedList = self.Food_List
         items = []
         for i in range(selectedList.count()):
             items.append(selectedList.item(i).text())
-
-
         for x in range(3):
             for y in range(7):
                 Rmeal = random.choice(items)
@@ -189,7 +195,7 @@ class MainPage(QWidget):
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': # run the GUI
     app = QApplication(sys.argv)
     Main = MainPage()
     Main.show()
